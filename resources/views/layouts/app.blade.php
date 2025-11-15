63bd289097612f39028da03107e6a25f19c6fcd0
zahwa01 <ribelpurca.mb@gmail.com>
2025-11-15 21:01:53 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
 </nav>
         @yield('content')
         <div class="text-center bg-light p-4 fixed-bottom">
-        &copy; Syahrul Ramadhan - 2310010498
+        &copy; Rizq Dzakhwa - 2310010395
         </div>
     <p></p>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
